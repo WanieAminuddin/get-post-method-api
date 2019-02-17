7fa8f3a19f461f52d954ccbc1f61e6541ef1cb0a
wanieaminuddin <46997096+WanieAminuddin@users.noreply.github.com>
2019-02-17 16:02:51 +0800

    Add files via upload

---
+++ LinkedinClient/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+using System;
+using System.Windows.Media.Imaging;
+using System.Windows.Controls;
+using LinkedinClient.LService;
+using LinkedInOauth.Models;
+
+
+namespace LinkedinClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        linkedinServicedataClient client = new linkedinServicedataClient();
+        string xmlprofile;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void btnGetProfile_Click(object sender, RoutedEventArgs e)
+        {
+            summary.Text += client.linkedinProfile();
+            xmlprofile = summary.Text;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            person profile = new person();
+            XmlSerializer serializer = new XmlSerializer(typeof(person));
+            XmlReader reader = XmlReader.Create(new StringReader(xmlprofile));
+            profile = (person)serializer.Deserialize(reader);
+
+            profileBox.Text += System.Environment.NewLine + "Name   :   " + profile.FName + " " + profile.LName;
+            profileBox.Text += System.Environment.NewLine + "Location   :   " +  profile.location.name;
+            profileBox.Text += System.Environment.NewLine + "Position   :   " + profile.headline;
+            profileBox.Text += System.Environment.NewLine + "No. of Connections :   " + profile.connections.ToString();
+            profileBox.Text += System.Environment.NewLine + "Summary    :   " + profile.summary;
+            
+            
+        }
+    }
+}
---
+++ LinkedinClient/ProfileData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Xml.Serialization;
+using System.Xml;
+
+
+namespace LinkedInOauth.Models
+{
+    public class person
+    {
+        [XmlElement("first-name")]
+        public string FName;
+
+        [XmlElement("last-name")]
+        public string LName;
+      
+        [XmlElement("picture-url")]
+        public string imageurl;
+       
+        [XmlElement("num-connections")]
+        public int connections;
+
+        [XmlElement("name")]
+        public string place;
+
+        public string headline;
+        public string summary;
+
+        [XmlElement("location")]
+        public location location;
+
+    }
+    public class location
+    {
+        public string name;
+    }
+
+}
+
